1c4e3901484b03ae0a3d9a1afe28f3d6be1b18c3
SanzhIss <200103158@stu.sdu.edu.kz>
2023-02-27 00:00:37 +0600

    updates

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 .PHONY: build
 build:
 	go build -v ./cmd/apiserver
-	
+
 .PHONY: test
 test:
-	go test -v -race -timeout 30s ./...
+	go test -v -race -timeout 30s ./...cre	
 
 .DEFAULT_GOAL := build
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -4,17 +4,20 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/Bexultan200103484/http-rest-api/internal/app/store"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
 // APIServer ...
-type APIServer struct{
+type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
-func New(config *Config) *APIServer{
+
+func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
@@ -22,17 +25,20 @@ func New(config *Config) *APIServer{
 	}
 }
 
-func (s*APIServer) Start() error {
+func (s *APIServer) Start() error {
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
 	s.configureRouter()
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 	s.logger.Info("starting api server")
 	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 func (s *APIServer) configureLogger() error {
 	level, err := logrus.ParseLevel(s.config.LogLevel)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	s.logger.SetLevel(level)
@@ -42,9 +48,19 @@ func (s *APIServer) configureLogger() error {
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
 }
-func (s *APIServer) handleHello() http.HandlerFunc{
-	
-	return func(w http.ResponseWriter, r *http.Request){
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+	return nil
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello")
 	}
 }
